9c581e0afe093a904970292e2ad3433b8f6c08db
tomkys144 <tomkys144@gmail.com>
2019-03-22 15:04:15 +0100

    changed config to YAML file

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
+import yaml
 from skautis import SkautisApi
+with open("config.yml", "r") as config:
+    cfg = yaml.load(config)
 
 
 # API call
-from .Config import key
-
-skautis = SkautisApi(appId=key, test=True)
+skautis = SkautisApi(appId=cfg['key'], test=True)
 
 
 # children units IDs
@@ -12,14 +13,11 @@ unit_id = int()
 
 
 def unit_ids():
-    from .Config import login
-    from .Config import unit
-    units_id = skautis.OrganizationUnit.UnitAllUnit(ID_Login=login, ID_Unit=unit)
+    units_id = skautis.OrganizationUnit.UnitAllUnit(ID_Login=cfg['login'], ID_Unit=cfg['unit'])
     return units_id
 
 
 # list
 def person_list():
-    from .Config import login
-    plist = skautis.OrganizationUnit.PersonAllExport(ID_Login=login, ID_Unit=unit_id)
+    plist = skautis.OrganizationUnit.PersonAllExport(ID_Login=cfg['login'], ID_Unit=unit_id)
     return plist
