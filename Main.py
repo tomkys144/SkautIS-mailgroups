f60840ce630f00bff51655a878ef80d0f9626401
tomkys144 <tomkys144@gmail.com>
2019-03-22 10:26:24 +0100

    functions

--- Main.py
+++ Main.py
@@ -1,14 +1,15 @@
 from skautis import SkautisApi
 
-#API call
+
+# API call
 from .Config import APIKEY
 
 skautis = SkautisApi(APIKEY, test=True)
 
-#list
-from .Config import uni
-from .Config import login
 
-class Peli(self):
-    list = skautis.OrganizationUnit.PersonAllExport(ID_Login=login, ID_Unit=uni)
-    return list
+# list
+def peli():
+    from .Config import uni
+    from .Config import login
+    perlis = skautis.OrganizationUnit.PersonAllExport(ID_Login=login, ID_Unit=uni)
+    return perlis
