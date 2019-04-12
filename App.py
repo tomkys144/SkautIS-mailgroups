6b9de2a8b710b687ecd95155a02273077b862edb
tomkys144 <tomkys144@gmail.com>
2019-04-12 12:31:46 +0200

    added id_list

--- App.py
+++ App.py
@@ -10,5 +10,23 @@ skautis = SkautisApi(appId=cfg['key'], test=True)
 
 # person list
 def person_list(login, unit, direct):
-    plist = skautis.OrganizationUnit.PersonAll(ID_Login=login, ID_Unit=unit, OnlyDirectMember=direct,ID=None, ID_FunctionType=None, ID_QualificationType=None)
+    plist = skautis.OrganizationUnit.PersonAll(
+        ID_Login=login, ID_Unit=unit, OnlyDirectMember=direct,ID=None, ID_FunctionType=None, ID_QualificationType=None
+    )
     return plist
+
+
+def id_list(person_list, login):
+    idlist = []
+    for x in person_list:
+        units = skautis.OrganizationUnit.MembershipAllPerson(
+            ID_Login=login, ID_Person=x['ID']
+        )
+        for y in units:
+            Unit = y['Unit']
+            ID_Unit = y['ID_Unit']
+        idlist.append([x['ID'], Unit, ID_Unit])
+    return idlist
+
+
+print(person_list(cfg['login'], cfg['unit'], True))
