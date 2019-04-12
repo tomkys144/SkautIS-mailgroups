042c02d29e1b8b558da063a8fb5a1b68d9d21af8
tomkys144 <tomkys144@gmail.com>
2019-04-12 08:06:22 +0200

    complete restructure

--- App.py
+++ App.py
@@ -9,18 +9,7 @@ with open("config.yml", "r") as config:
 skautis = SkautisApi(appId=cfg['key'], test=True)
 
 
-# children units IDs
-def unit_ids():
-    uids = []
-    units_info = array(skautis.OrganizationUnit.UnitAll(ID_Login=cfg['login'], ID_UnitParent=cfg['unit']))
-    i = 0
-    for x in units_info:
-        uids[i] = x["ID"]
-        i = i+1
-    return uids
-
-
-# list
-def person_list(uid):
-    plist = skautis.OrganizationUnit.PersonAllExport(ID_Login=cfg['login'], ID_Unit=uid)
+# person list
+def person_list():
+    plist = skautis.OrganizationUnit.PersonAll(ID_Login=cfg['login'], ID_Unit=cfg['unit'], OnlyDirectMember=False)
     return plist
