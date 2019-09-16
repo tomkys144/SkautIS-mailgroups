34dde85fa2398348a89025c0da2f2d0e0a59a926
tomkys144 <tomas.kysela@gbl.cz>
2019-09-16 08:15:55 +0200

    Added logging

--- App.py
+++ App.py
@@ -4,10 +4,24 @@ from converter import converter
 from google.oauth2 import service_account
 import googleapiclient.discovery
 import json
+import logging
+import datetime
 with open('config.yml', 'r') as config:
     cfg = yaml.load(config)
 with open('./conf/gkey.json', 'r') as google_key:
     gkey = json.loads(google_key)
+
+today = datetime.date.today()
+ftoday = today.strftime("%d/%m/%Y")
+now = datetime.time.now()
+fnow = now.strftime("%H/%M/%S")
+
+
+name = '/log/log_{0}_{1}'.format(ftoday, fnow)
+log = logging.getLogger(__name__)
+fhandler = logging.FileHandler(name)
+log.setLevel(logging.INFO)
+log.addHandler(fhandler)
 
 credentials = service_account.Credentials.from_service_account_file(google_key)
 gservice = googleapiclient.discovery.build('admin', 'directory_v1', credentials=credentials)
@@ -84,33 +98,36 @@ def checker(login, unit):
                         skautis.GoogleApps.GoogleGroupUpdateMemberRole(
                             ID_Login=login, ID=group['ID'], Email=gkey['client_email'], IsOwner=True
                         )
+                        log.info('Service account {0} set as owner of group {1}'.format(gkey['client_email'], group['DisplayName']))
                 if 'Rodiče' not in group['DisplayName']:
                     missing = set(contacts['members']).difference(group_mails)
                     for email in missing:
                         skautis.GoogleApps.GoogleGroupUpdateMemberEmail(
                             ID_Login=login, ID=group['ID'], EmailArray=email
                         )
+                        log.info('Email {0} added to group {1]'.format(email, group['DisplayName']))
                     additional = set(group_mails).difference(contacts['members'])
                     for email in additional:
                         skautis.GoogleApps.GoogleGroupDeleteMember(
                             ID_Login=login, ID=group['ID'], Email=email
                         )
+                        log.info('Email {0} removed from group {1]'.format(email, group['DisplayName']))
                 elif 'Rodiče' in group['DisplayName']:
                     missing = set(contacts['parents']).difference(group_mails)
                     for email in missing:
                         skautis.GoogleApps.GoogleGroupUpdateMemberEmail(
                             ID_Login=login, ID=group['ID'], EmailArray=email
                         )
+                        log.info('Email {0} added to group {1]'.format(email, group['DisplayName']))
                     additional = set(group_mails).difference(contacts['parents'])
                     for email in additional:
                         skautis.GoogleApps.GoogleGroupDeleteMember(
                             ID_Login=login, ID=group['ID'], Email=email
                         )
+                        log.info('Email {0} removed from group {1]'.format(email, group['DisplayName']))
                 else:
-                    raise Exception(
-                        '''Invalid name of Ggroup!
-                        unit: {0}
-                        group name: {1}'''.format(unt['DisplayName'], group['DisplayName'])
+                    log.error(
+                        'Invalid name of Ggroup! | unit: {0} | group name: {1}'.format(unt['DisplayName'], group['DisplayName'])
                                     )
             else:
                 mail = converter(unt['DisplayName'])
@@ -118,26 +135,40 @@ def checker(login, unit):
                     ID_Login=login, ID_Unit=unt['ID'], DisplayName="Rodiče - " + unt['DisplayName'],
                     Email="rodice-" + mail + "@" + cfg['domain']
                 )
+                log.info('Created group {}'.format("Rodiče - " + unt['DisplayName']))
                 skautis.GoogleApps.GoogleGroupUpdateMemberEmail(
                     ID_Login=login, ID=new_id, EmailArray=contacts['parents']
                 )
+                log.info('Added these emails to the group {0}: {1}'.format(
+                    "Rodiče - " + unt['DisplayName'], contacts['parents']
+                ))
                 skautis.GoogleApps.GoogleGroupUpdateMemberEmail(
                     ID_Login=login, ID=new_id, EmailArray=gkey['client_email']
                 )
                 skautis.GoogleApps.GoogleGroupUpdateMemberRole(
                     ID_Login=login, ID=group['ID'], Email=gkey['client_email'], IsOwner=True
                 )
+                log.info('Added {0} into group {1} as owner'.format(
+                    gkey['client_email'], "Rodiče - " + unt['DisplayName']
+                ))
                 new_id = skautis.GoogleApps.GoogleGroupInsert(
                     ID_Login=login, ID_Unit=unt['ID'], DisplayName=unt['DisplayName'],
                     Email=mail + "@" + cfg['domain']
                 )
+                log.info('Created group {}'.format(unt['DisplayName']))
                 skautis.GoogleApps.GoogleGroupUpdateMemberEmail(
                     ID_Login=login, ID=new_id, EmailArray=contacts['members']
                 )
+                log.info('Added these emails to the group {0}: {1}'.format(
+                    unt['DisplayName'], contacts['parents']
+                ))
                 skautis.GoogleApps.GoogleGroupUpdateMemberEmail(
                     ID_Login=login, ID=new_id, EmailArray=gkey['client_email']
                 )
                 skautis.GoogleApps.GoogleGroupUpdateMemberRole(
                     ID_Login=login, ID=group['ID'], Email=gkey['client_email'], IsOwner=True
                 )
+                log.info('Added {0} into group {1} as owner'.format(
+                    gkey['client_email'], unt['DisplayName']
+                ))
 
