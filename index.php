e2bfa1a7fef807e1d647113c646de915a620801e
tomkys144 <tomas.kysela@gbl.cz>
2019-11-27 14:55:51 +0100

    Started working on frontend (plugin for WP). Sends settings.

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ *Plugin name: Skautis Mailgroups
+ * Version 0.1
+ * Author: Tomáš Kysela
+ * License: The Unlicence
+ */
+$login_link = '';
+class groups{
+    public function init(){
+        \add_shortcode('groups', [$this, 'groupsShortCode']);
+    }
+
+    private function input($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    public function groupsShortCode() {
+        $unit = $domain = '';
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $unit = input($_POST['unit']);
+            $domain = input($_POST['domain']);
+        }
+        $url = 'http://groups.tkysela.cz/setup';
+        $package = array('unit' => $unit, 'domain' => $domain);
+        $data = json_encode($package);
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) { /* Handle error */ }
+
+        var_dump($result);
+    }
+}
+?>
+<form method="post">
+    <table>
+        <tr>
+            <td>Jednotka:</td>
+            <td><input type = "text" name = "unit"></td>
+        </tr>
+
+        <tr>
+            <td>Doména:</td>
+            <td><input type = "text" name = "domain"></td>
+        </tr>
+    </table>
+</form>
+<a href=>START</a>
